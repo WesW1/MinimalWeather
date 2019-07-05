b925c14a8c9fc50b89a7a9376e72aedf4fcd60b0
Wes Ward <wesward132@gmail.com>
2019-07-05 00:39:39 -0400

    api call http -> https

--- app/util/api.js
+++ app/util/api.js
@@ -3,8 +3,8 @@ const key = '71a695c99fdc312a3b3e6c908cd48005'
 
 
 export function fetchWeather (city) {
-  const endpoint = new URL(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`)
-  
+  const endpoint = new URL(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`)
+
   return fetch(endpoint)
     .then((res) => res.json())
     .then((data) => {
